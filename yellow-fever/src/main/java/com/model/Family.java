5719b49a9f33bd3658b9079f38af657b7f1f6dd0
emanoelvianna <emanoel.vianna@outlook.com>
2018-07-13 13:44:06 -0300

    model - inicial commit

---
+++ yellow-fever/src/main/java/com/model/Family.java
@@ -0,0 +1,9 @@
+package com.model;
+
+import sim.util.Bag;
+
+public class Family {
+
+  private Bag members;
+  private Residence location;
+}
---
+++ yellow-fever/src/main/java/com/model/Residence.java
@@ -0,0 +1,8 @@
+package com.model;
+
+public class Residence {
+
+  private int xLocation;
+  private int yLocation;
+
+}
